c35af6d1afd2caa75a0767ab6504cc3e83948f95
Jake Kaufman <me@jake.computer>
2019-12-02 01:27:32 -0500

    day 2 part 2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -9,39 +9,56 @@ fn main() {
         process::exit(1);
     }
     let input = fs::read_to_string(&args[1]).expect("Unable to read file");
-    let mut ops = input
+    let original_ops = input
         .trim()
         .split(',')
         .map(|x| x.parse::<usize>().expect("Failed to parse file!"))
         .collect::<Vec<usize>>();
-    let mut pc = 0;
 
-    while pc < ops.len() {
-        println!("{}", ops[pc]);
-        match ops[pc] {
-            1 => {
-                let to = ops[pc + 3];
-                ops[to] = ops[ops[pc + 1]] + ops[ops[pc + 2]];
-                pc += 4
+    let mut noun = 0;
+    let mut verb = 0;
+    println!("noun {}, verb {}", noun, verb);
+    loop {
+        let mut ops = original_ops.clone();
+        ops[1] = noun;
+        ops[2] = verb;
+        let mut pc = 0;
+
+        while pc < ops.len() {
+            match ops[pc] {
+                1 => {
+                    let to = ops[pc + 3];
+                    ops[to] = ops[ops[pc + 1]] + ops[ops[pc + 2]];
+                    pc += 4
+                }
+                2 => {
+                    let to = ops[pc + 3];
+                    ops[to] = ops[ops[pc + 1]] * ops[ops[pc + 2]];
+                    pc += 4
+                }
+                99 => {
+                    break;
+                }
+                _ => {
+                    println!("error");
+                    break;
+                }
             }
-            2 => {
-                let to = ops[pc + 3];
-                ops[to] = ops[ops[pc + 1]] * ops[ops[pc + 2]];
-                pc += 4
-            }
-            99 => {
-                println!("break");
-                break;
-            }
-            _ => {
-                println!("error");
-                break;
+        }
+        println!(
+            "{}",
+            ops.iter()
+                .fold("".to_string(), |acc, x| format!("{},{}", acc, x))
+        );
+        if ops[0] == 19690720 {
+            println!("{}", 100 * ops[1] + ops[2]);
+            break;
+        } else {
+            noun += 1;
+            if noun > 99 {
+                noun = 0;
+                verb += 1;
             }
         }
     }
-    println!(
-        "{}",
-        ops.iter()
-            .fold("".to_string(), |acc, x| format!("{},{}", acc, x))
-    );
 }
