6af09857c6d7392f34eb6920edc614eb2b3e1b8d
Jake Kaufman <me@jake.computer>
2019-12-15 13:35:11 -0500

    day 5 pt 2

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -36,43 +36,99 @@ fn main() {
     let mut ops = original_ops.clone();
 
     let mut pc = 0;
-    let mut input = vec![1];
+    let mut input = vec![5];
+    println!(
+        "{}",
+        input
+            .iter()
+            .fold("".to_string(), |acc, x| format!("{},{}", acc, x))
+    );
     let mut output: Vec<i32> = Vec::new();
 
     while pc < ops.len() {
         let op = ops[pc];
-        let a = digit_n(op, 4);
-        let b = digit_n(op, 3);
-        let c = digit_n(op, 2);
         let opcode = op % 100;
+        let param1_mode = digit_n(op, 2);
+        let param2_mode = digit_n(op, 3);
+        let param3_mode = digit_n(op, 4);
         match opcode {
             1 => {
+                // add param1 * param2 store at param3
                 // The 3 parameter can never be in immediate mode
-                assert!(a == 0);
-                let a_pos = ops[pc + 3] as usize;
-                let b_pos = get_pos(b, pc + 2, &ops);
-                let c_pos = get_pos(c, pc + 1, &ops);
-                ops[a_pos] = ops[c_pos] + ops[b_pos];
+                assert!(param3_mode == 0);
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                let param2 = get_pos(param2_mode, pc + 2, &ops);
+                let param3 = ops[pc + 3] as usize;
+                ops[param3] = ops[param1] + ops[param2];
                 pc += 4
             }
             2 => {
+                // multiply param1 * param2 store at param3
                 // The 3 parameter can never be in immediate mode
-                assert!(a == 0);
-                let a_pos = ops[pc + 3] as usize;
-                let b_pos = get_pos(b, pc + 2, &ops);
-                let c_pos = get_pos(c, pc + 1, &ops);
-                ops[a_pos] = ops[c_pos] * ops[b_pos];
+                assert!(param3_mode == 0);
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                let param2 = get_pos(param2_mode, pc + 2, &ops);
+                let param3 = ops[pc + 3] as usize;
+                ops[param3] = ops[param1] * ops[param2];
                 pc += 4
             }
             3 => {
-                let pos = get_pos(c, pc + 1, &ops);
-                ops[pos] = input.pop().expect("Tried to read input and failed");
+                // input store param1
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                ops[param1] = input.pop().expect("Tried to read input and failed");
                 pc += 2
             }
             4 => {
-                let pos = get_pos(c, pc + 1, &ops);
-                output.push(ops[pos]);
+                // output from param1
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                output.push(ops[param1]);
                 pc += 2
+            }
+            5 => {
+                // jump-if-true if param1 != 0, pc = param2, else continue
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                let param2 = get_pos(param2_mode, pc + 2, &ops);
+                if ops[param1] != 0 {
+                    pc = ops[param2] as usize
+                } else {
+                    pc += 3
+                }
+            }
+            6 => {
+                // jump-if-false if param1 == 0, pc = param2, else continue
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                let param2 = get_pos(param2_mode, pc + 2, &ops);
+                if ops[param1] == 0 {
+                    pc = ops[param2] as usize
+                } else {
+                    pc += 3
+                }
+            }
+            7 => {
+                // less than if param1 < param2, ops[param3] = 1 else ops[param3] = 0
+                assert!(param3_mode == 0);
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                let param2 = get_pos(param2_mode, pc + 2, &ops);
+                let param3 = ops[pc + 3] as usize;
+                if ops[param1] < ops[param2] {
+                    ops[param3] = 1
+                } else {
+                    ops[param3] = 0
+                }
+                pc += 4
+            }
+            8 => {
+                // equal to if param1 == param2, ops[param3] = 1 else ops[param3] = 0
+                assert!(param3_mode == 0);
+                let param1 = get_pos(param1_mode, pc + 1, &ops);
+                let param2 = get_pos(param2_mode, pc + 2, &ops);
+                let param3 = ops[pc + 3] as usize;
+                if ops[param1] == ops[param2] {
+                    ops[param3] = 1
+                } else {
+                    ops[param3] = 0
+                }
+                pc += 4
             }
             99 => {
                 break;
@@ -82,6 +138,12 @@ fn main() {
                 break;
             }
         }
+        println!(
+            "{}",
+            ops[pc..ops.len()]
+                .iter()
+                .fold("".to_string(), |acc, x| format!("{},{}", acc, x))
+        );
     }
     println!(
         "{}",
